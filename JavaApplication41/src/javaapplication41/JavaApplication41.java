aedfda755c8f6e5db6c226fd255b365d6ecab584
tungnpl <52806435+tungnpl@users.noreply.github.com>
2019-12-16 08:52:35 +0700

    First Commit

---
+++ JavaApplication41/src/javaapplication41/JavaApplication41.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication41;
+
+/**
+ *
+ * @author tungn
+ */
+public class JavaApplication41 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
